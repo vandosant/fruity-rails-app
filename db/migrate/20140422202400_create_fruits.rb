24e8811dc6ef3841d5c792af45507386bad5233b
vandosant <sjskender@gmail.com>
2014-04-23 11:41:57 -0600

    Updated migration to syntax convention

--- db/migrate/20140422202400_create_fruits.rb
+++ db/migrate/20140422202400_create_fruits.rb
@@ -1,8 +1,8 @@
 class CreateFruits < ActiveRecord::Migration
   def change
     create_table :fruits do |t|
-      t.column :name, :string
-      t.column :description, :string
+      t.string :name
+      t.string :description
     end
   end
 end
